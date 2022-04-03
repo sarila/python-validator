af03332a481b40fb787960bab9d4b69b4b7a17bc
Sarila <40534147+sarila@users.noreply.github.com>
2022-04-03 21:05:44 +0545

    Python Validator

--- Validation with optional rules.py
+++ Validation with optional rules.py
@@ -1,145 +1,107 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[20]:
+# In[58]:
 
 
 dict_to_validate = {"name": "Sarila Ngakhusi", "address": "Chyamasingh, Bhaktapur", "age": 20, "score": 95.6,  "covid": False}
+# dict_to_validate = {}
 # rules={}
 # rules = {
-#     "name": {"data_type": "str", "min_length": 6, "max_length": 15},
-#     "address": {"data_type": "str", "min_length": 10, "max_length": 50}, 
-#     "age" : {"data_type": "int", "min_value": 18, "max_value": 40}, 
-#     "score" : {"data_type": "float", "min_value": 40}
+#     "name": {"data_type": "str", "min": 6, "max": 15},
+#     "address": {"data_type": "str", "min": 10, "max": 50}, 
+#     "age" : {"data_type": "int", "min": 18, "max": 40}, 
+#     "score" : {"data_type": "float", "min": 40}
 # }
 rules = { 
-    "age" : {"data_type": "int", "min_value": 18, "max_value": 40}, 
-    "score" : {"data_type": "float", "min_value": 40}
+    "age" : {"data_type": "int", "min": 18, "max": 40}, 
+    "score" : {"data_type": "float", "min": 40},
+#     "height": {"data_type": "float", "min": 5}
 }
 result = []
-def string_validator(string_to_validate, rules):
-#     print(rules)
-    if isinstance(string_to_validate, str) :
-        string_length = len(string_to_validate)
-        if "min_length"in rules and "max_length" in rules:
-            if string_length >= rules["min_length"] and string_length <= rules["max_length"]:
+def validation_logic(item_to_validate, rules) :
+    if "min"in rules and "max" in rules:
+        if item_to_validate >= rules["min"] and item_to_validate <= rules["max"]:
                 return True
-            else:
-                return False
-        elif "min_length" in rules:
-            if string_length >= rules["min_length"]:
-                return True
-            else: 
-                return False
-        elif "max_length" in rules:
-            if string_length <= rules["max_length"]:
-                return True
-            else: 
-                return False
         else:
+            return False
+    elif "min" in rules:
+        if item_to_validate >= rules["min"]:
             return True
-    else: 
-        print(f"{string_to_validate} must be string")
-        return False
-    
-def int_validator(number_to_validate, rules):
-    if isinstance(number_to_validate, int):
-        if "min_value"in rules and "max_value" in rules:
-            if number_to_validate >= rules["min_value"] and number_to_validate <= rules["max_value"]:
-                return True
-            else:
-                return False
-        elif "min_value" in rules:
-            if number_to_validate >= rules["min_value"]:
-                return True
-            else: 
-                return False
-        elif "max_value" in rules:
-            if number_to_validate <= rules["max_value"]:
-                return True
-            else: 
-                return False
-        else:
+        else: 
+            return False
+    elif "max" in rules:
+        if item_to_validate <= rules["max"]:
             return True
-    else :
-        print(f"{number_to_validate} must be integer")
-        return False
-    
-def float_validator(number_to_validate, rules):
-    if isinstance(number_to_validate, float):
-        if "min_value"in rules and "max_value" in rules:
-            if number_to_validate >= rules["min_value"] and number_to_validate <= rules["max_value"]:
-                return True
-            else:
-                return False
-        elif "min_value" in rules:
-            if number_to_validate >= rules["min_value"]:
-                return True
-            else: 
-                return False
-        elif "max_value" in rules:
-            if number_to_validate <= rules["max_value"]:
-                return True
-            else: 
-                return False
-        else:
-            return True
+        else: 
+            return False
     else:
-        print(f"{number_to_validate} must be float")
-        return False
-    
-def boolean_validator(bool_to_validate):
-    if bool_to_validate == True or bool_to_validate == False:
         return True
-    else:
-        return False
-    
+def validator(item_to_validate, rules):
+#     print(rules)
+#     if is string
+    if rules["data_type"] == "str":
+        
+        if isinstance(item_to_validate, str) :
+            item_to_validate = len(item_to_validate)
+            validation_logic(item_to_validate, rules)
+        else: 
+            print(f"{item_to_validate} must be string")
+            return False
+    elif rules["data_type"] == "int":
+#     if the number is integer
+        if isinstance(item_to_validate, int):
+            validation_logic(item_to_validate, rules)
+        else :
+            print(f"{item_to_validate} must be integer")
+            return False
+    if rules["data_type"] == "float":
+#     if float 
+        if isinstance(item_to_validate, float):
+            validation_logic(item_to_validate, rules)
+        else:
+            print(f"{item_to_validate} must be float")
+            return False
 
 def validate_generic_data(dict_to_validate, rules):
     for each in dict_to_validate:
         if each not in rules:
-            if type(dict_to_validate[each]) == str:
-                res = string_validator(dict_to_validate[each], rules)
-                if res == False:    
-                    result.append(each)
-            if type(dict_to_validate[each]) == int:
-                res = int_validator(dict_to_validate[each], rules)
-                if res == False:    
-                    result.append(each)
-            if type(dict_to_validate[each]) == float:
-                res = float_validator(dict_to_validate[each], rules)
-                if res == False:    
-                    result.append(each)
-            if type(dict_to_validate[each]) == bool:
-                res = boolean_validator(dict_to_validate[each])
-                if res == False:
-                    result.append(res)
+            res = validator(dict_to_validate, {"data_type": type(dict_to_validate[each])})
+            if res == False:
+                result.append(res)
         else :
-            if rules[each]["data_type"] == "str":
-                res = string_validator(dict_to_validate[each], rules[each])
-                if res == False:    
-                    result.append(each)
-            if rules[each]["data_type"] == "int":
-                item = rules[each]
-                res = int_validator(dict_to_validate[each], rules[each])
-                if res == False:    
-                    result.append(each)
-            if rules[each]["data_type"] == "float":
-                res = float_validator(dict_to_validate[each], rules[each])
-                if res == False:    
-                    result.append(each)
-            
+            res = validator(dict_to_validate[each], rules[each])
+            if res == False:    
+                result.append(each)  
     if not result:
         return True
     else:
         return False
-        
-validated = validate_generic_data(dict_to_validate, rules)
-if validated:
-    print("True: The data are validated")
-else :
-    for item in result:
-        print(item + " doesnot follow the validation rule")
+
+def equivalent_rules_dict(dict_to_validate, rules):
+    if not dict_to_validate:
+        print("Dictionary is empty: No values to validate")
+    else:
+        for each in rules:
+            if each not in dict_to_validate:
+                return False
+
+        validated = validate_generic_data(dict_to_validate, rules)
+        if validated:
+            print("True: The data are validated")
+        else :
+            for item in result:
+                print(item + " doesnot follow the validation rule")
+
+rules_and_keys = equivalent_rules_dict(dict_to_validate, rules)
+if rules_and_keys == False:
+    print("All the rules are not associated with the keys in dictionary to be validated")
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
